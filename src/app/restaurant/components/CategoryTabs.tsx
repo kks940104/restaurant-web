dc5b1951b95891527fdf91b7c3a29b3a78a715ee
kks940104 <kks940104@gmail.com>
2025-02-11 13:03:09 +0900

    ..

--- src/app/restaurant/components/CategoryTabs.tsx
+++ src/app/restaurant/components/CategoryTabs.tsx
@@ -3,8 +3,36 @@ import React, { useState, useLayoutEffect } from 'react'
 import { getCategories } from '../services/actions'
 import styled from 'styled-components'
 import { CommonType } from '@/app/global/types/StyledType'
+import classNames from 'classnames'
+import colors from '@/app/global/styles/colors'
+import sizes from '@/app/global/styles/sizes'
 
-const StyledNav = styled.nav<CommonType>``
+const { primary, white, dark, info } = colors
+const { medium } = sizes
+
+const StyledNav = styled.nav<CommonType>`
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 30px;
+  .tab {
+    border: 1px solid ${primary};
+    color: ${white};
+    padding: 10px 15px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: ${medium};
+    margin-bottom: 5px;
+    background: ${info};
+    &.on {
+      background: ${primary};
+      color: ${white};
+      border: 1px solid ${dark};
+    }
+  }
+  .tab + .tab {
+    margin-left: 5px;
+  }
+`
 
 const CategoryTabs = ({ categories, onClick }) => {
   const [items, setItems] = useState<string[]>()
@@ -16,7 +44,22 @@ const CategoryTabs = ({ categories, onClick }) => {
     })()
   }, [])
 
-  return <StyledNav></StyledNav>
+  return (
+    items &&
+    items.length > 0 && (
+      <StyledNav>
+        {items.map((item) => (
+          <div
+            key={'category_' + item}
+            onClick={() => onClick(item)}
+            className={classNames('tab', { on: categories.includes(item) })}
+          >
+            {item}
+          </div>
+        ))}
+      </StyledNav>
+    )
+  )
 }
 
 export default React.memo(CategoryTabs)
